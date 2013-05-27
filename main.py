b111f89744bdcfd58bdbc5abb21ed1b3ef1479a5
scottmiao <jazzymiao@gmail.com>
2013-05-27 09:31:15 +0800

    Listing Songs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, session, redirect, url_for, \
     abort, render_template, flash
+from models import Song
 
 
 app = Flask(__name__)
@@ -21,10 +22,17 @@ def contact(title=TITLE):
     return render_template('contact.html', title=title)
 
 
+# Listing Songs
+@app.route('/songs')
+def show_songs(title=TITLE):
+    songs = Song.query.all()
+    return render_template('songs.html', songs=songs, title=title)
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('not_found.html'), 404
 
 
 if __name__ == '__main__':
-    app.run(debug='True')
+    app.run(debug='True', host='0.0.0.0')
