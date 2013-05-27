c760058f1e7f0004b5ecdfb8e5751f8147ff6a25
Scott Miao <jazzymiao@gmail.com>
2013-05-27 20:58:32 +0800

    Create a new song, delete a song, add "edit_song"
    TODO: "edit_song.html" template

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, session, redirect, url_for, \
     abort, render_template, flash
-from models import Song
+from models import Song, db
+from datetime import date
 
 
 app = Flask(__name__)
@@ -28,11 +29,45 @@ def show_songs(title=TITLE):
     songs = Song.query.all()
     return render_template('songs.html', songs=songs, title=title)
 
+
 # showing a song
 @app.route('/songs/<int:song_id>')
 def show_a_song(song_id, title=TITLE):
     song = Song.query.filter(Song.id == song_id).first()
     return render_template('show_song.html', song=song, title=title)
+
+
+# add a new song
+@app.route('/songs/new', methods=['GET', 'POST'])
+def new_song(title=TITLE):
+    if request.method == 'POST':
+        lst = (request.form['released_on']).split('/')
+        released_on = date(int(lst[2]), int(lst[1]), int(lst[0]))
+        song = Song(request.form['title'],
+                    request.form['lyrics'],
+                    request.form['length'],
+                    released_on)
+        db.session.add(song)
+        db.session.commit()
+        return redirect(url_for('show_songs'))
+    else:
+        return render_template('new_song.html', title=title)
+
+
+# delete a song
+@app.route('/songs/<int:song_id>/delete', methods=['POST', 'DELETE'])
+def delete_song(song_id, title=TITLE):
+    song = Song.query.filter(Song.id == song_id).first()
+    db.session.delete(song)
+    db.session.commit()
+    return redirect(url_for('show_songs'))
+
+
+# edit and update a song
+@app.route('/songs/<int:song_id>/edit')
+def edit_song(song_id, title=TITLE):
+    song = Song.query.filter(Song.id == song_id).first()
+    return render_template('edit_song.html', song=song, title=title)
 
 
 @app.errorhandler(404)
